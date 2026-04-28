b5c6bf802e9d52bf26f119177a26ed389222f14d
isa-lara <isalarabella1119@gmail.com>
2026-04-27 21:20:28 -0500

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,16 @@
+#Inicializando variables y colocandoles un valor
+nombre= isabella lara cuadro
+edad= 13
+Estado= True
+
+#Esta linea me imprime el contenido de una variable
+print("Bienvenido", isabella)
+print("Tu edad es", 13)
+
+#Uso de la funcion input()
+nombre=input("¿Cual es tu nombre? ")
+edad=input("¿Cual es tu edad? ")
+
+#Mostrar el resultado capturado con input()
+print("Bienvenido", nombre)
+print("Tu edad es", edad)
